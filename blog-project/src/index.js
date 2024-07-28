d5f6e76ae29174b0a0674872f1e3f7e18b231375
Alyssa Ortiz <ao994@nau.edu>
2024-07-28 14:34:11 -0700

    installed new dependencies, created secrets

--- blog-project/src/index.js
+++ blog-project/src/index.js
@@ -1,3 +1,7 @@
+//import secrets
+import * as Secrets from 'secrets';
+
+//import dependencies
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
