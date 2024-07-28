b1d2272021f08fedc510ea22f052df183e73d1d7
Alyssa Ortiz <ao994@nau.edu>
2024-07-28 15:28:47 -0700

    moving things around

--- blog-project/src/index.js
+++ blog-project/src/index.js
@@ -1,13 +1,28 @@
 //import secrets
-import * as Secrets from 'secrets';
+import { MONGODB_LOGIN } from "secrets.js";
 
 //import dependencies
+import express from "express";
+import cors from "cors";
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
+
+//constants
+const mongoose = require("mongoose");
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+//connect to database
+mongoose.connect(MONGODB_LOGIN);
 
 //changed; ReactDOM.render is depreciated
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
 
-
+// start the Express server
+app.listen(PORT, () => {
+  console.log('Server listening on port ${PORT}');
+});
